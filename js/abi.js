60b4305704bb5dca71690ec4773969bc2712860f
aishwar18 <61742418+aishwar18@users.noreply.github.com>
2020-06-28 12:07:39 +0530

    Update abi.js

--- js/abi.js
+++ js/abi.js
@@ -1,4 +1,5 @@
 
+var hsSC="0x6f78992f89FC4640B465de5bBA0bfd13559d64e3";
 var hsABI=[
 	{
 		"anonymous": false,
@@ -146,19 +147,13 @@ var hsABI=[
 				"type": "bytes32"
 			},
 			{
-				"internalType": "address",
-				"name": "ui_dev_addr",
-				"type": "address"
+				"internalType": "bytes32",
+				"name": "ui_dev_id",
+				"type": "bytes32"
 			}
 		],
 		"name": "add_dev",
-		"outputs": [
-			{
-				"internalType": "bool",
-				"name": "",
-				"type": "bool"
-			}
-		],
+		"outputs": [],
 		"stateMutability": "nonpayable",
 		"type": "function"
 	},
@@ -230,10 +225,34 @@ var hsABI=[
 	{
 		"inputs": [
 			{
+				"internalType": "string",
+				"name": "new_occ",
+				"type": "string"
+			},
+			{
 				"internalType": "address payable",
-				"name": "old_own",
+				"name": "new_occ_add",
 				"type": "address"
 			},
+			{
+				"internalType": "bytes32",
+				"name": "house_id",
+				"type": "bytes32"
+			}
+		],
+		"name": "change_curr_occ",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
 			{
 				"internalType": "string",
 				"name": "new_owner",
@@ -534,6 +553,25 @@ var hsABI=[
 				"type": "bytes32"
 			}
 		],
+		"name": "dev_ret_curr_occ_addr",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "bytes32",
+				"name": "id",
+				"type": "bytes32"
+			}
+		],
 		"name": "dev_ret_home_det",
 		"outputs": [
 			{
@@ -565,6 +603,25 @@ var hsABI=[
 				"internalType": "uint256",
 				"name": "",
 				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "bytes32",
+				"name": "id",
+				"type": "bytes32"
+			}
+		],
+		"name": "dev_ret_house_own_addr",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
 			}
 		],
 		"stateMutability": "view",
@@ -753,7 +810,9 @@ var hsABI=[
 		"type": "function"
 	}
 ]
-var hsSC="0xFe388911E1039a4eA1F62a90282D2a2b15E95bC1";
+
+
+var usSC="0xba73B19502bDa9b0C75CC8D048a862152A031a8E";
 
 var usABI=[
 	{
@@ -790,6 +849,11 @@ var usABI=[
 	{
 		"inputs": [
 			{
+				"internalType": "address",
+				"name": "a",
+				"type": "address"
+			},
+			{
 				"internalType": "bytes32",
 				"name": "house_id",
 				"type": "bytes32"
@@ -808,16 +872,6 @@ var usABI=[
 	},
 	{
 		"inputs": [
-			{
-				"internalType": "string",
-				"name": "new_owner",
-				"type": "string"
-			},
-			{
-				"internalType": "address",
-				"name": "new_owner_add",
-				"type": "address"
-			},
 			{
 				"internalType": "bytes32",
 				"name": "house_id",
@@ -861,19 +915,6 @@ var usABI=[
 	},
 	{
 		"inputs": [],
-		"name": "dev_ret_homelist",
-		"outputs": [
-			{
-				"internalType": "bytes32[]",
-				"name": "",
-				"type": "bytes32[]"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [],
 		"name": "dev_ret_user",
 		"outputs": [
 			{
@@ -897,7 +938,7 @@ var usABI=[
 	}
 ]
 
-var usSC="0xBD6AfA8624AA823a230D744052bAA1Fc01659A1A";
+var sdSC="0xd259eCE5988705db1bf37d7655e26a38AB3Fd31b";
 
 var sdABI=[
 	{
@@ -960,54 +1001,6 @@ var sdABI=[
 			}
 		],
 		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "bytes32",
-				"name": "id",
-				"type": "bytes32"
-			}
-		],
-		"name": "dev_ret_sec",
-		"outputs": [
-			{
-				"internalType": "bytes32",
-				"name": "",
-				"type": "bytes32"
-			},
-			{
-				"internalType": "bytes32",
-				"name": "",
-				"type": "bytes32"
-			},
-			{
-				"internalType": "enum securitydevices.STYPES",
-				"name": "",
-				"type": "uint8"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "bytes32",
-				"name": "id",
-				"type": "bytes32"
-			}
-		],
-		"name": "grant_perm",
-		"outputs": [
-			{
-				"internalType": "bytes32",
-				"name": "",
-				"type": "bytes32"
-			}
-		],
-		"stateMutability": "view",
 		"type": "function"
 	},
 	{
@@ -1080,7 +1073,459 @@ var sdABI=[
 		],
 		"stateMutability": "nonpayable",
 		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "bytes32",
+				"name": "id",
+				"type": "bytes32"
+			}
+		],
+		"name": "dev_ret_sec",
+		"outputs": [
+			{
+				"internalType": "bytes32",
+				"name": "",
+				"type": "bytes32"
+			},
+			{
+				"internalType": "bytes32",
+				"name": "",
+				"type": "bytes32"
+			},
+			{
+				"internalType": "enum securitydevices.STYPES",
+				"name": "",
+				"type": "uint8"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "bytes32",
+				"name": "id",
+				"type": "bytes32"
+			}
+		],
+		"name": "grant_perm",
+		"outputs": [
+			{
+				"internalType": "bytes32",
+				"name": "",
+				"type": "bytes32"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
 	}
 ]
 
-var sdSC="0xae7Cf917FAa546E2dC415413fbAD9d1bF37EE624";
+var emrgdevAbi = [
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: false,
+        internalType: "bytes32",
+        name: "house_id",
+        type: "bytes32",
+      },
+      {
+        indexed: false,
+        internalType: "bytes32",
+        name: "emrg_dev_id",
+        type: "bytes32",
+      },
+    ],
+    name: "alert_emergency",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: false,
+        internalType: "bytes32",
+        name: "",
+        type: "bytes32",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    name: "emit_Units_Consumed",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: false,
+        internalType: "bytes32",
+        name: "house_id",
+        type: "bytes32",
+      },
+      {
+        indexed: false,
+        internalType: "bytes32",
+        name: "emrg_dev_id",
+        type: "bytes32",
+      },
+    ],
+    name: "registered_emrg_dev",
+    type: "event",
+  },
+  {
+    inputs: [
+      {
+        internalType: "bytes32",
+        name: "ui_house_id",
+        type: "bytes32",
+      },
+      {
+        internalType: "bytes32",
+        name: "ui_emrg_dev_id",
+        type: "bytes32",
+      },
+    ],
+    name: "emergency",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "ui_access_id",
+        type: "address",
+      },
+      {
+        internalType: "bytes32",
+        name: "ui_emrg_dev_id",
+        type: "bytes32",
+      },
+    ],
+    name: "grant_access",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "bytes32",
+        name: "ui_emrg_dev_id",
+        type: "bytes32",
+      },
+      {
+        internalType: "uint256",
+        name: "ui_emrg_units_con",
+        type: "uint256",
+      },
+    ],
+    name: "read_units_consumed",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "bytes32",
+        name: "ui_house_id",
+        type: "bytes32",
+      },
+      {
+        internalType: "uint256",
+        name: "ui_emrg_dev_rating",
+        type: "uint256",
+      },
+      {
+        internalType: "string",
+        name: "ui_emrg_dev_type",
+        type: "string",
+      },
+    ],
+    name: "register_emergency_device",
+    outputs: [
+      {
+        internalType: "bytes32",
+        name: "",
+        type: "bytes32",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "ui_access_id",
+        type: "address",
+      },
+      {
+        internalType: "bytes32",
+        name: "ui_emrg_dev_id",
+        type: "bytes32",
+      },
+    ],
+    name: "remove_access",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "bytes32",
+        name: "ui_emrg_dev_id",
+        type: "bytes32",
+      },
+    ],
+    name: "retrieve_emrg_dev_details",
+    outputs: [
+      {
+        internalType: "bytes32",
+        name: "",
+        type: "bytes32",
+      },
+      {
+        internalType: "enum Emergency_Device.DEVTYPES",
+        name: "",
+        type: "uint8",
+      },
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "bytes32",
+        name: "ui_emrg_dev_id",
+        type: "bytes32",
+      },
+    ],
+    name: "retrieve_mapping",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+];
+var emrgdevAddr = "0x835F18136f4129d554c796E160B182796943b474";
+
+var emrgDevTopics = {reg:"0x91d7dc9651593afc25ee0f6012ad44246c8774061126a5003ddb70f38502cba0",
+					unit:"0x9ff4d64aa877498c19c687f36af1d0ca03ea8857054d4e87b74bd59c971cba20",
+					emrg:"0x81a398a552d2b57303f587ca935e666a2362dbbdab6714a9707b3e267f70ae44"}
+var utdevAbi = [
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: false,
+        internalType: "bytes32",
+        name: "",
+        type: "bytes32",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    name: "emit_Units_Consumed",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: false,
+        internalType: "bytes32",
+        name: "house_id",
+        type: "bytes32",
+      },
+      {
+        indexed: false,
+        internalType: "bytes32",
+        name: "emrg_dev_id",
+        type: "bytes32",
+      },
+    ],
+    name: "registered_ut_dev",
+    type: "event",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "ui_access_id",
+        type: "address",
+      },
+      {
+        internalType: "bytes32",
+        name: "ui_ut_dev_id",
+        type: "bytes32",
+      },
+    ],
+    name: "grant_access",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "bytes32",
+        name: "ui_ut_dev_id",
+        type: "bytes32",
+      },
+      {
+        internalType: "uint256",
+        name: "ui_ut_units_con",
+        type: "uint256",
+      },
+    ],
+    name: "read_units_consumed",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "bytes32",
+        name: "ui_house_id",
+        type: "bytes32",
+      },
+      {
+        internalType: "uint256",
+        name: "ui_ut_dev_rating",
+        type: "uint256",
+      },
+      {
+        internalType: "string",
+        name: "ui_ut_dev_type",
+        type: "string",
+      },
+    ],
+    name: "register_utility_device",
+    outputs: [
+      {
+        internalType: "bytes32",
+        name: "",
+        type: "bytes32",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "ui_access_id",
+        type: "address",
+      },
+      {
+        internalType: "bytes32",
+        name: "ui_ut_dev_id",
+        type: "bytes32",
+      },
+    ],
+    name: "remove_access",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "bytes32",
+        name: "ui_ut_dev_id",
+        type: "bytes32",
+      },
+    ],
+    name: "retrieve_ut_dev_details",
+    outputs: [
+      {
+        internalType: "bytes32",
+        name: "",
+        type: "bytes32",
+      },
+      {
+        internalType: "enum Utility_Device.DEVTYPES",
+        name: "",
+        type: "uint8",
+      },
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+      {
+        internalType: "string",
+        name: "",
+        type: "string",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "bytes32",
+        name: "ui_ut_dev_id",
+        type: "bytes32",
+      },
+      {
+        internalType: "string",
+        name: "ui_ut_last_ser_date",
+        type: "string",
+      },
+    ],
+    name: "service_device",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+];
+
+var utdevAddr = "0xA04c18b9cfE3495eF4066517Dc3500d0d8f7B028";
+
+var utDevTopics = {
+  reg:"0xc752657b29309768e63435e256a3b089013dff5bb8c2e161796401cffa9b4d46",
+  unit:"0x9ff4d64aa877498c19c687f36af1d0ca03ea8857054d4e87b74bd59c971cba20",
+};
