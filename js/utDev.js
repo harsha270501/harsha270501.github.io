4071d00c5a52b9dee2b28e40dbd5bb585dd4206e
Roopa Vidhya G <61742380+random3900@users.noreply.github.com>
2020-06-28 17:03:13 +0530

    Update utDev.js

--- js/utDev.js
+++ js/utDev.js
@@ -13,7 +13,7 @@ function hexToString(hex) {
 }
 //get details
 function getdetails() {
-  ut_dev_id = $("#ut-dev-id").value;
+  ut_dev_id = $("#ut-dev-id");
     console.log(ut_dev_id);
   try {
     var myContract = new web3.eth.Contract(utdevAbi, utdevAddr, {
