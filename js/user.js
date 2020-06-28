b0a365a1303c75b5fbc9f123a929296de1bbcf96
harsha270501 <61742450+harsha270501@users.noreply.github.com>
2020-06-28 22:37:51 +0530

    Update user.js

--- js/user.js
+++ js/user.js
@@ -1,23 +1,60 @@
 web3 = new Web3(new Web3.providers.WebsocketProvider("wss://ropsten.infura.io/ws/v3/cbd9dc11b30147e9a2cc974be655ef7c"));
          
-
-
-var emergdevuser = web3.eth.subscribe('logs', {
-             address: '0xDB39318510c207DD7AA1710355662ff566380AB6',
-             topics: ['0x0a4324ff0f4987efc8378421cf6f6bb4d65afce7e749384d8c5ca1c4bee8bd7a']
-             }, function(error, result){
-             console.log("inside if");   
-             if (!error)
-             {
-                console.log(result.data);
+var Emergency = web3.eth.subscribe(
+  "logs",
+  {
+    address: emrgdevAddr,
+    topics: [emrgDevTopics.emrg],
+  },
+  function (error, result) {
+    var datar=result.data;
+    var ini="0x";
+    var res1=ini.concat(datar.slice(2,66));
+    var res2=ini.concat(datar.slice(66));
+    console.log(res1); //house id
+    console.log(res2); //device id
+    confirmationPopUp("Device ID:"+res2);
+    console.log("inside if");
+    if (!error) {
+      console.log(result);
+      var addr;
+                try 
+                {
+                // contract Abi defines all the variables,constants and functions of the smart contract. replace with your own abi
+                //instantiate and connect to contract address via Abi
+                var myContract = new web3.eth.Contract(hsABI, hsSC, {from: account, gasPrice: '5000000', gas:'3000000'});
+        
                 
-             }
-             else
-             {
-                console.log(error);
-             }
-         });
+        
+                myContract.methods.dev_ret_curr_occ_addr(res2).send(function (err, result) 
+                {
+                    if (err) 
+                    { 
+                        console.log(err);
+                    }
+                    if (result) 
+                    {
+                        //display value on the webpage
+                        console.log(result);
+                        addr=result;
+                    }
+                });
 
+               }
+            catch (err) 
+            {
+              console.log(err);
+            }
+                      if(addr==account)
+                      {
+                               confirmationPopup("Emergency Device ALert");
+                      }
+      
+    } else {
+      console.log(error);
+    }
+  }
+);
 var monitdevuser = web3.eth.subscribe('logs', {
              address: '0x0c9D4008DdC91409aFabaFe6b6211D83313591f1',
              topics: ['0x32e7399906561f7bfc72dac3412790f6ac770339cd768936dcf5c7ec126eba47']
