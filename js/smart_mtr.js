135106e924b3652096cb95344bcfa0b6bbb4a4f1
aishwar18 <61742418+aishwar18@users.noreply.github.com>
2020-06-28 16:27:59 +0530

    Update smart_mtr.js

--- js/smart_mtr.js
+++ js/smart_mtr.js
@@ -8,7 +8,8 @@ var register_mtr = web3.eth.subscribe(
   "logs",
   {
     address: smart_mtr_sca,
-    topics: smart_mtr_topics.reg,
+    topics: 0xdcca2fd466d95919391212a9e3c71b1ebabf78ac15dd2b33ce4531e03242be6c
+};
   },
   function (error, result) 
     {
@@ -82,7 +83,7 @@ var unit_consumed = web3.eth.subscribe(
   "logs",
   {
     address: smart_mtr_sca,
-    topics: smart_mtr_topics.token,
+    topics: 0x63740acb0b61ea9006ac46884aa42fc7cb582744fe9d34048797b599be5e0d33,
   },
   function (error, result) {
     console.log("inside if");
