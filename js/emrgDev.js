a3ae5575e282108dc9c311f3bdce10551d21837a
Roopa Vidhya G <61742380+random3900@users.noreply.github.com>
2020-06-28 19:13:06 +0530

    Update emrgDev.js

--- js/emrgDev.js
+++ js/emrgDev.js
@@ -27,7 +27,7 @@ function getdetails(){
               document.getElementById("r1").innerHTML=result[0];
               house_id = document.getElementById("r1").innerHTML = result[0];
               document.getElementById("r2").innerHTML=result[2];
-              document.getElementById("r3").innerHTML=Arr[result[2]];
+              document.getElementById("r3").innerHTML=Arr[result[1]];
               document.getElementById("r4").innerHTML=result[3];
               document.getElementById("display-div").className ="show";
 
